0adccb9fe0c627edb76c2eb7c7b7ce4df5ba2871
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-03-08 15:14:30 +0100

    Declare variables

--- program.py
+++ program.py
@@ -1,1 +1,10 @@
 from ortools.sat.python import cp_model
+
+model = cp_model.CpModel()
+
+# vars declaration 3x4
+x = {}
+for r in range(3):
+    for c in range(4):
+        x[(r,c)] = model.new_int_var(1, 12, f"x_{r}_{c}")
+
